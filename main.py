34fba4be99d548324fc1e9b2c60548ba442405f2
Fueziwa <k24130kk@aitech.ac.jp>
2025-10-23 15:58:35 +0900

    main.pyの修正

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import transcript
-import Record, write
+import Record
 from transcript import transcribe_audio
-if __name__ == '__main__':
-    
+from write import save_transcript
+
+if __name__ == '__main__': 
     filename = Record.recording()
     #filenameは、ファイルの名前を参照する際の変数
     print(filename)
+    
+    #transcribed_textは、文字起こしされた文字用の変数
     transcribed_text = transcribe_audio()
-    write()
+
+    save_transcript(transcribed_text)
