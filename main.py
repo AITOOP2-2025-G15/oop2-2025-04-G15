a427bd814aa516b14d34982852c5ab893f673c31
Fueziwa <k24130kk@aitech.ac.jp>
2025-10-23 15:53:44 +0900

    write.pyの修正、main.pyの追加

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import Record, transcribe, write
+import transcript
+import Record, write
+from transcript import transcribe_audio
 if __name__ == '__main__':
     
     filename = Record.recording()
     #filenameは、ファイルの名前を参照する際の変数
     print(filename)
-    transcribe()
+    transcribed_text = transcribe_audio()
     write()
